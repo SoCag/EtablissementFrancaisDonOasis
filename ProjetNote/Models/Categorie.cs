06f7e54415305c001068d847cdb99a736fd7c59f
assee <assee@CIELPHIS>
2023-12-22 12:47:59 +0100

    Mise en place du formuliare principal ( la connexion) + du formulaire d'inscription du donnateur + redirection des bon form.

---
+++ ProjetNote/Models/Categorie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetNote.Models;
+
+public partial class Categorie
+{
+    public int IdCategorie { get; set; }
+
+    public string Ordre { get; set; } = null!;
+
+    public string Categorie1 { get; set; } = null!;
+
+    public virtual ICollection<Question> Questions { get; set; } = new List<Question>();
+}
---
+++ ProjetNote/Models/Donneur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetNote.Models;
+
+public partial class Donneur
+{
+    public int IdDonneur { get; set; }
+
+    public string AdresseEmail { get; set; } = null!;
+
+    public string MotDePasse { get; set; } = null!;
+
+    public string Nom { get; set; } = null!;
+
+    public string Prenom { get; set; } = null!;
+
+    public DateOnly DateDeNaissance { get; set; }
+
+    public virtual ICollection<Questionnaire> Questionnaires { get; set; } = new List<Questionnaire>();
+}
