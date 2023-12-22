406862e5d9fbb204e3416e95bf164928ceeb5e19
Nolan C <nolan.collard0803@gmail.com>
2023-12-22 15:38:21 +0100

    fddfg

---
+++ ProjetNote/Models/ViewDataGridQuestionnaire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetNote.Models;
+
+public partial class ViewDataGridQuestionnaire
+{
+    public int IdQuestionnaire { get; set; }
+
+    public string Question { get; set; } = null!;
+
+    public string Reponse { get; set; } = null!;
+
+    public string? Complement { get; set; }
+
+    public int RendImpossible { get; set; }
+}
